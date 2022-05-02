e3480e79a2ad0a0118a1f38157df4eaf2022b94e
Abel Granados <infoabelgranados@gmail.com>
2022-05-02 15:46:19 -0400

    la condicion de salida es n < 1024 en lugar de <FDT> que podia presentar una falla en caso de que los ultimos 5 bytes sean divididos por el tamaño del buffer. Adicionalmente se comprueban los bytes de extremo a extremo para comprobar que se transmitio el archivo por completo

--- client.go
+++ client.go
@@ -28,19 +28,15 @@ func main(){
 	//evio informacion del archivo al servidor
 	// conn.Write([]byte(arInfo.Name())) 
 	// conn.Write([]byte(string(arInfo.Size()))) //el error de impresion tiene que ver con UTF-8
-	var count int64
 	//Envio el archivo
-	n64 , err := io.Copy(conn, ar)
-	count+=n64
+	n , err := io.Copy(conn, ar)
 	if err != nil{
 		log.Println("El archivo no se pudo enviar.")
 	}
-	FDT := "<FDT>" //Fin de Transmisión
-	//si le envio al servidor la cantidad exacta de bytes que debe esperar entonces sabra cuando terminar
-	n, _ := conn.Write([]byte(FDT))
-	count+=int64(n)
 	log.Println("Archivo enviado")
-	log.Println("Bytes enviados", count)
+	log.Println("Bytes enviados", n)
+
+	
 	thisCli := bufio.NewReader(os.Stdin)
 	for {
 		msg, _, _ := thisCli.ReadLine()
--- fileServer.go
+++ fileServer.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"net"
 	"os"
-	"io"
 )
 
 
@@ -34,52 +33,33 @@ func nombreTemp()(nombre string){
 func hCliente(conn net.Conn){
 	log.Println("Cliente conectado: ", conn.RemoteAddr().String())
 
-	//creo el archivo que voy a llenar
 	arlocal, err := os.Create("archivo")
 	if err != nil{
 		log.Println("No se pudo crear la ruta local para el archivo")
 	}
 	defer arlocal.Close() 
-	//io.Copy no funcina apropiadamente para leer de la conexion y llenar el archivo
+	
 	buffer := make([]byte,1024)
-	//read lee de la conexion de forma infinita, para que se detenga en caso de EOF
-	FDT := "<FDT>" //Fin de Transmisión
-	r := io.Reader(conn)
 	var count int
 	for {
-		n, err := r.Read(buffer)
+		n, err := conn.Read(buffer)
 		count+=n
-		// log.Print(n)
-		//leo los ultimos 5 bytes para ver si se presenta FDT
-		//<FDT> tiene 5 bytes
-		if string(buffer[n-5:n]) == FDT{
-			_, aerr := arlocal.Write(buffer[:n-5])
-			if aerr != nil{
-				//eliminar el archivo porque no se pudo crear correctamente
-				log.Println(aerr)
-				break
-			}
-			log.Println("Archivo recivido")
-			break
-		}
 
 		if err != nil{
-			//la condicio io.EOF no funciona. Debe ser porque en una conexion no funciona la condicion io.EOF, o esa condicion no se envia desde el cliente.
-			//a persar de que no funciona la dejo aquí para ver si puedo hacer que funcione
-			if n == 0 && err == io.EOF{
-				log.Println("Archivo recivido")
-				break
-			}
-			//Eliminar archivo porque no se pudo cargar por completo
-			//Puede ser un error de conexion
-			log.Println(err)
-			break
+			//posible desconexion
+			//se debe eliminar el archivo
+			log.Fatal(err)
 		}
 	
 		_, aerr := arlocal.Write(buffer[:n])
 		if aerr != nil{
 			//eliminar el archivo porque no se pudo crear correctamente
 			log.Println(aerr)
+			break
+		}
+
+		if n < 1024 {
+			log.Println("Archivo recivido")
 			break
 		}
 }
@@ -89,29 +69,6 @@ func hCliente(conn net.Conn){
 
 
 
-	// for {
-	// 	bc, err := conn.Read(buffer) //lee bytes de 1024 en 1024
-	// 	if err != nil{
-	// 		log.Println(err)
-	// 		//elimino el archivo temporal actual
-	// 		return
-	// 	}
-		
-	// 	n, err := arlocal.Write(buffer[:bc])
-	// 	if err != nil{
-	// 		// if n != len(buffer){
-	// 		// 	log.Println("no se escribieron todos los bytes")
-	// 		// } 
-	// 		log.Println("fallo al escribir el archivo")
-	// 		return
-	// 	}
-	// 	log.Println("bytes escritos: ", n)	
-	// }
-
-	// log.Println("Archivo recivido")
-	// 	//el error de impresion tiene que ver con el UTF-8
-	// 	log.Println("Mensaje: ",(string(b[:bc]))) 
-	// }
 	conn.Close()
 	log.Println("Servidor cerrado")
 }
