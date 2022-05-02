9eaca0149bfbcec9822e313a248817cf8c7e30eb
Abel Granados <infoabelgranados@gmail.com>
2022-05-02 19:52:50 -0400

    ultimos cambios de hoy. Logre comunicar el comnado -subir- y usar un canal para enviar respuestas al cliente. Despues me concentrare en simplificar las cosas, por ahora es mejor lograr la comunicacion

--- client.go
+++ client.go
@@ -3,10 +3,10 @@ package main
 import(
 	"net"
 	"log"
-	"bufio"
+	// "bufio"
 	"os"
 	"io"
-	"strings"
+	// "strings"
 )
 
 
@@ -17,30 +17,38 @@ func main(){
 		log.Fatal(err)
 	}
 	log.Println("Cliente activo")
-	archivo := "pic.png"
-	//Si el nombre del archivo tiene espacios los elimino con TrimSpace
-	ar, err := os.Open(strings.TrimSpace(archivo))
-	if err != nil{
-		log.Println("El archivo no se pudo leer")
-	}
-	defer ar.Close()
-	// arInfo, err := ar.Stat()
-	//evio informacion del archivo al servidor
-	// conn.Write([]byte(arInfo.Name())) 
-	// conn.Write([]byte(string(arInfo.Size()))) //el error de impresion tiene que ver con UTF-8
-	//Envio el archivo
-	n , err := io.Copy(conn, ar)
-	if err != nil{
-		log.Println("El archivo no se pudo enviar.")
-	}
-	log.Println("Archivo enviado")
-	log.Println("Bytes enviados", n)
+	// archivo := "pic.png"
+	// //Si el nombre del archivo tiene espacios los elimino con TrimSpace
+	// ar, err := os.Open(strings.TrimSpace(archivo))
+	// if err != nil{
+	// 	log.Println("El archivo no se pudo leer")
+	// }
+	// defer ar.Close()
+	// // arInfo, err := ar.Stat()
+	// //evio informacion del archivo al servidor
+	// // conn.Write([]byte(arInfo.Name())) 
+	// // conn.Write([]byte(string(arInfo.Size()))) //el error de impresion tiene que ver con UTF-8
+	// //Envio el archivo
+	// n , err := io.Copy(conn, ar)
+	// if err != nil{
+	// 	log.Println("El archivo no se pudo enviar.")
+	// }
+	// log.Println("Archivo enviado")
+	// log.Println("Bytes enviados", n)
 
-	
-	thisCli := bufio.NewReader(os.Stdin)
-	for {
-		msg, _, _ := thisCli.ReadLine()
-		conn.Write(msg)
+	go respuestasServidor(conn)
+
+	//la función de aqui abajo parece crear un flujo desde el Stdin a la conexion, no es como que lea lo que hay en stdin y lo envie si no que el
+	//flujo siempre esta abierto.
+	_, err = io.Copy(conn, os.Stdin)
+	if err != nil{
+		log.Fatal(err)
 	}
 	conn.Close()
+}
+
+
+func respuestasServidor(conn net.Conn){
+	//escribe las respuestas del servidor en al terminal
+	io.Copy(os.Stdout, conn)
 }
--- fileServer.go
+++ fileServer.go
@@ -4,6 +4,9 @@ import (
 	"log"
 	"net"
 	"os"
+	"bufio"
+	"strings"
+	"fmt"
 )
 
 
@@ -29,46 +32,81 @@ func nombreTemp()(nombre string){
 	return "archivo"
 }
 
+const BUFFER_TAMANIO int = 1024
 //runtina para comunicarce con el cliente
 func hCliente(conn net.Conn){
+	defer conn.Close() //me aseguro de cerra la conexion en cualquier caso
 	log.Println("Cliente conectado: ", conn.RemoteAddr().String())
-
-	arlocal, err := os.Create("archivo")
-	if err != nil{
-		log.Println("No se pudo crear la ruta local para el archivo")
+	canal := make(chan string)
+	go responderCliente(conn, canal) //para enviar las respuestas del servidor al cliente
+	lector := bufio.NewScanner(conn)
+	for lector.Scan(){
+		entrada := lector.Text()
+		interprete(entrada, conn, canal)
 	}
-	defer arlocal.Close() 
-	
-	buffer := make([]byte,1024)
-	var count int
-	for {
-		n, err := conn.Read(buffer)
-		count+=n
-
-		if err != nil{
-			//posible desconexion
-			//se debe eliminar el archivo
-			log.Fatal(err)
-		}
-	
-		_, aerr := arlocal.Write(buffer[:n])
-		if aerr != nil{
-			//eliminar el archivo porque no se pudo crear correctamente
-			log.Println(aerr)
-			break
-		}
-
-		if n < 1024 {
-			log.Println("Archivo recivido")
-			break
-		}
-}
-	//los bytes recividos deben coincidir con los enviados
-	log.Println("Bytes recividos", count)
-
-
-
-
-	conn.Close()
 	log.Println("Servidor cerrado")
+}
+
+func responderCliente(conn net.Conn, chanel <-chan string){
+	for msg := range chanel{
+		//esta función formatea el string de msg de forma estandar y lo escribe en la conexion
+		log.Println("Se escribio el mensaje en al conexion")
+		fmt.Fprintln(conn, msg) //ingnoro los errores
+	}
+}
+
+func interprete(entrada string, conn net.Conn, canal chan string){
+	var comando []string
+	//msg guarda el texto mientras recorro todo el contenido en el canal
+	log.Println(entrada)
+	comando = strings.Split(entrada, " ")
+	switch(comando[0]){
+	case "subir":
+		log.Println("Comando subir recivido")
+		canal<- "server: Comando subir reconocido, espera mientras mientras se arregla todo en el servidor"
+		subirArchivo(conn, canal)
+	case "obtener":
+		log.Println("comando obtener recivido")
+	default:
+		log.Println(entrada, "no es un comando valido")
+	}
+	return
+}
+
+
+func subirArchivo(conn net.Conn, canal chan string){
+		archivo, err := os.Create("archivo")
+		if err != nil{
+			log.Println("No se pudo crear la ruta local para el archivo")
+			canal <- "Error en el servidor, no se puede subir el archivo"
+			return
+		}
+		defer archivo.Close() 
+		canal <- "server: Listo parar recivir archivo"
+		buffer := make([]byte, BUFFER_TAMANIO)
+		var cuenta int 
+		for {
+			n, err := conn.Read(buffer)
+			cuenta+=n
+
+			if err != nil{
+				//posible desconexion
+				//se debe eliminar el archivo
+				log.Fatal(err)
+			}
+		
+			_, aerr := archivo.Write(buffer[:n])
+			if aerr != nil{
+				//eliminar el archivo porque no se pudo crear correctamente
+				log.Println(aerr)
+				break
+			}
+
+			if n < BUFFER_TAMANIO {
+				log.Println("Archivo recivido")
+				break
+			}
+	}
+		//los bytes recividos deben coincidir con los enviados
+		log.Println("Bytes recividos", cuenta)
 }
