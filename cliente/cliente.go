83f969f8605f04a1532ca537ed2e2f7df24ab2f4
Abel Granados <infoabelgranados@gmail.com>
2022-05-17 09:48:10 -0400

    Cambios de ayer, se me olvido cargarlos

--- cliente/cliente.go
+++ cliente/cliente.go
@@ -1,204 +1,50 @@
 package main
 
 import(
-	Ps "protocolo_simple"
+	ioc "ioCliente"
 	"net"
 	"log"
 	"bufio"
 	"os"
-	"io"
 	"strings"
 	"fmt"
 )
 
+
 var estado string //los estados posibles del cliente CONECTADO, UNIDO_A_CANAL, ACORDANDO_TRANSMISION
 
-//Una structura a manera de abstracción para representar un conjunto de funciones que generan salidas
-//a la red y reciven entradas. Son procesos intermediario entre el servidor y el cliente que 
-//recive una orden y determina la forma de usar el protocolo simple para cumplirla. 
-//Los resultados varian dependiendo de la orden recivida.
-type IoCliente struct{
-	conn net.Conn
-}
-
-
-//Utilisa el protocolo para crear la peticion adecuada y enviarla. Despues espera la respuesta
-//y notifica el resultado obtenido
-func (ioCli *IoCliente) UnirCanal(nombreCanal string)(exito bool, error string){
-	//estado = "CONECTADO"
-	
-	var campos = map[string]string{"nombreCanal":nombreCanal} //campos necesarios para esta petición
-
-	peticion, serr := Ps.NuevaPeticion(ioCli.conn, "unirCanal", campos)
-	if serr != "" {
-		log.Println("DEBUG:", serr)
-		return
-	}
-	//la peticion paso la prueba
-	serr = peticion.Enviar() 
-	if serr != "" {
-		log.Println("DEBUG:", serr)
-		return 
-	}
-
-	rsp, err := peticion.RecivirRespuesta()
-	if err != ""{
-		log.Println("DEBUG:", err)
-		return 
-	}
-
-	if rsp[0] == Ps.S_UNIR_ACEPTADO{
-		return true, "" //exito
-	}else if rsp[0] == Ps.S_UNIR_RECHAZADO{
-		return false,"" //no exito xd
-	}else{
-		log.Println("DEBUG: Se recivio una respuesta incoerente")
-		return false, "La respuesta recivida no es acorde a la peticon"
-	}
-
-}
-
-//Funcion de prueba para enviar archivos al canal
-func (ioC *IoCliente) EnviarArchivo(rutaArchivo, nombreArchivo, pesoArchivo, nombreCanal string){
-	
-	//primero sin usar el protocolo a manera de prueba
-	// 1. enviar el archivo al servidor
-	// 2. enviar al canal
-	// 3. enviar al servidor y hacer echo al cliente sobre el canal lul!
- 	peticion := Ps.C_ENVIAR_ARCHIVO + " " + nombreCanal + " " + nombreArchivo + " " + pesoArchivo + "\n"
-	
- 	n, err := ioC.conn.Write([]byte(peticion))
- 	if err != nil{
- 		log.Println(err)
- 		return
- 	}
- 	log.Println("Petición para enviar archivo enviada al servidor, escrito ", n)
-
-
- 	//verifico si acepto o rechazo el archivo
- 	var buff [512]byte //da igual si lo hago un slice de una vez
- 	n, err = ioC.conn.Read(buff[:])
- 	if err != nil{
- 		log.Println("Error al leer la respuesta del servidor:", err)
- 		return
- 	}
-
- 	temp := string(buff[:n])
- 	rsp := strings.Trim(temp, "\n")
- 	rsps := strings.Split(rsp," ")
- 	if rsps[0] == Ps.S_ENVIO_RECHAZADO{
- 		log.Println("El servidor no acepto la transferecia")
- 		return
- 	}
-
-	if rsps[0] != Ps.S_ENVIO_APROBADO{
-		log.Println("Respuesta invalida. El servidor respondio con:", rsp)
-		return
-	}
-
-	log.Println("El servidor acepto la transferencia del archivo")	
-
-	//Le envio el archivo
-	ar, err := os.Open(rutaArchivo)
-	if err != nil{
-		log.Println(err)
-		return
-	}
-	defer ar.Close()
-
-	bc, err := io.Copy(ioC.conn, ar)
-	if err != nil{
-		
-		log.Println("El archivo no se pudo enviar.")
-		log.Println(err)
-		return
-
-	}else{
-
-		//espera confirmación de recepcion de archivo
-		n, err = ioC.conn.Read(buff[:])
-	 	if err != nil{
-	 		log.Println("Error al leer la respuesta del servidor:", err)
-	 		return
-	 	}
-	 	rsp := string(buff[:n])
-	 	rsp = strings.Trim(rsp, "\n") 
-	 	rsps := strings.Split(rsp," ")
-	 	if rsps[0] != Ps.S_ARCHIVO_RECIVIDO{
-	 		log.Println("Erro, el servidor respondion con:", rsp)	
-	 		return
- 		}
- 		log.Println("El servidor indica que recivio el archivo y pide confirmacion")
-	 	log.Println("El mensaje del servidor es:", rsps[1:])
-
-		
-		//Notifico al servidor que efectivamente envie todo el archivo
-		n, err = ioC.conn.Write([]byte(Ps.C_ARCHIVO_ENVIADO+"\n"))
- 		if err != nil {
- 			log.Println(err)
- 			return
- 		}
-
- 		log.Println("Confirmacion enviada::", Ps.C_ARCHIVO_ENVIADO)
-		log.Println("Bytes enviados:", bc)
-		log.Println("Lado del cliente termino")
-	}
-	//hasta aquí la primera prueba, que llegue al servidor
-}
-
-
-//Una funcion para probar si el canal recive. Despues sera borrada
-func (ioCli *IoCliente) ToChan(msg string){
-	//fuera de protocolo
-	n, err := ioCli.conn.Write([]byte(msg+"\n"))
-	if err != nil{
-		log.Println("El mensaje no se pudo enviar por el canal")
-	}
-	log.Println("Bytes escritos", n)
-
-	//leer la respuesta
-	var niceEcho [512]byte 
-	n, err = ioCli.conn.Read(niceEcho[:])
-	if err != nil{
-		log.Println(err)
-		return
-	}
-	log.Println("FROM_S:",string(niceEcho[:n]))
-}
-
-
-
-
-
-var ioCliente IoCliente
+var ioCliente ioc.IoCliente //input ouput del cliente. Su deber es presentar las funciones para enviar a la red y recivir de la red, usando el <protoco simple>.
 
 
 func main(){
 
-	conn, err := net.Dial("tcp","127.0.0.1:9999")
+	conn, err := net.Dial("tcp","127.0.0.1:9999") //9999 es el puerto estandar para el <protocolo simple>
 	if err != nil{
 		log.Fatal(err)
 	}
 	defer conn.Close()
 	estado = "CONECTADO" 
 	log.Println("Conexion establecida con el servidor")
-	ioCliente.conn = conn
+	ioCliente.Conn = conn
 
+	//Comienso a recivir las entradas desde la terminal
 	terminal := bufio.NewScanner(os.Stdin)
 	for terminal.Scan(){
 		entrada := terminal.Text() //lee una cadena de texto hasta \n
-		validarEntrada(entrada) //un comando a la vez
+		validarEntrada(entrada) //un comando a la vez. Es sincrona.
 	}
 
 }
 
 
 //Recive las entradas desde la terminal para validarlas.
-//Ejecuta dos niveles de validación: valida el formato, y valida los parametros
+//Cada comando debería tener su funcion de validación.
+//dependiendo de la entrada se pueden tener dos niveles de validacion. Validación de formato y validación de
+//parametros
 func validarEntrada(entrada string){
 	divisionEntrada := strings.Split(entrada, " ")
 	comando := strings.ToUpper(divisionEntrada[0]) //no case sensitive
-	campos := make(map[string]string) //lis de campos que acompañan al comando, varian segun el comando
+	campos := make(map[string]string) //lista de campos que acompañan al comando, varian segun el comando, esto debería ser una global.
 
 	switch(comando){
 	case "UNIR":
@@ -246,7 +92,7 @@ func validarEntrada(entrada string){
 
 		nombreCanal := divisionEntrada[1]
 		nombreArchivo := arInfo.Name()
-		pesoArchivo := fmt.Sprintf("%d", arInfo.Size()) //el peso se envÍa en string
+		pesoArchivo := fmt.Sprintf("%d", arInfo.Size()) //el peso se envía en string
 		
 		campos["rutaArchivo"] = rutaArchivo
 		campos["nombreArchivo"] = nombreArchivo 
@@ -287,7 +133,7 @@ func validarEntrada(entrada string){
 
 
 	case "DEBUG":
-		debug()
+		ioc.MuestraRegistro()
 
 
 	default:
@@ -303,7 +149,7 @@ func validarEntrada(entrada string){
 
 
 //Utilisa el ioCliente para hacer las peticiones y determina que hacer con las respuestas.
-//Es como un controller en el modelo MVC
+//Su intención es ser como un controller en el modelo MVC
 func gestionar(orden string, campos map[string]string){
 	switch orden{
 	case "unir":
@@ -338,6 +184,3 @@ func gestionar(orden string, campos map[string]string){
 }
 
 
-func debug(){
-	log.Println(Ps.LOG)
-}
--- servidor/servidor.go
+++ servidor/servidor.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	Ps "protocolo_simple"
+	Ps "protocoloSimple"
 	"log"
 	"net"
 	"os"
@@ -24,8 +24,7 @@ import (
 
 
 const (
-	//buffer de lectura de archivos
-	BUFFER_TAMANIO = 1024
+	BUFFER_RCV_ARCHIVO = 1024 //buffer de lectura de archivos
 )
 
 
@@ -50,6 +49,8 @@ type Canal struct{
 
 	salir chan *Cliente	//recive todas las peticiones para salir de este canal
 
+	listo chan bool //para coordinar
+
 }
 
 
@@ -60,21 +61,32 @@ type Canal struct{
 func (can *Canal) Iniciar(){
 	log.Println("El", can.nombre," esta abierto")
 	for{
+	
 		select{
 		case cli := <-can.unir:
-			log.Println(can.nombre,":: Cliente recivido")
+		
 			can.clientes[cli] = true //el canal conoce al cliente ahora
-			cli.canal = can //el cliente conoce al canal ahora
+			cli.canal = can //el cliente conoce al canal ahora //esto beria ser responsabilidad del cliente, no del canal
+			log.Println(can.nombre,"::Se registro un cliente")
+			can.listo<- true //terminado, esto paralisa al canal, y como varios clientes estan en el mismo canal, si el canal se paralisa procesando a un cliente todos los demas clientes se bloquean tambien. corregir
 		
 		case cli := <-can.salir:
-			delete(can.clientes, cli)
+		
+			delete(can.clientes, cli) //el canal ya no conoce al cliente
+			cli.canal =  nil //el cliente ya no conoce a un canal //esto debe ser responsabilidad del cliente, no del canal
+			can.listo<- true //terminado, esto paralisa al canal, y como varios clientes estan en el mismo canal, si el canal se paralisa procesando a un cliente todos los demas clientes se bloquean tambien. corregir
 		
 		case msg := <-can.escribir:
+
+			log.Println(can.nombre,"redirigiendo",msg)
 			for cli := range can.clientes{
-				log.Println(can.nombre,"::redirigiendo",msg)
-				cli.escribir<-msg
+				cli.escribir<- can.nombre+"::"+msg
+				<-cli.listo //terminado, esto paralisa al canal, y como varios clientes estan en el mismo canal, si el canal se paralisa procesando a un cliente todos los demas clientes se bloquean tambien. corregir
 			}
+			can.listo<-true
+			log.Println(can.nombre,"msg redirigido!")
 		}
+	
 	}
 }
 
@@ -90,6 +102,8 @@ type Cliente struct{
 	
 	escribir chan string //Para escribir mensajes al cliente
 
+	listo chan bool //para sincronisar 
+
 	canales *Canales //Lista de canales que existen
 
 	canal *Canal //El canal done este cliente está registrado 
@@ -97,42 +111,39 @@ type Cliente struct{
 
 
 //lee los mensajes provenientes del cliente y los manda al interprete de comandos
+//esta rutina termina si el la conexion del cliente se desase
 func (cli *Cliente) Leer(){
 	lector := bufio.NewScanner(cli.conn)
 	for lector.Scan(){
 		entrada := lector.Text()
 		log.Println("Recivido::", entrada)
 		cli.Interpretar(entrada) //se interpreta uno a la vez, es sincrono, a diferencia de escribir que es asincrono
-		//cuando el interprete retorna se vuelven a escuchar entradas desde el cliente
+		log.Println("Escuchando nuevas peticiones...")
 	}
 }
 
-//Espera una replica del cliente, un solo mensaje que sera la respuesta para una coordinación interna
-//otro nombre puede ser msgCoordinacionInterna
-func (cli *Cliente) respuestaCliente() (s string){
-	lector := bufio.NewScanner(cli.conn)
-	if lector.Scan(){
-		entrada := lector.Text()
-		return entrada
-	}
-	return "Error de lectura en replicaCliente()"
-}
 
 //Toma todos los mensaje para este cliente y se los envía
+//esta rutina termina si el canal para escribir se cierra
 func (cli *Cliente) Escribir(){
+
 	for msg := range cli.escribir{
 		_, err := fmt.Fprintln(cli.conn, msg) //se ignoran los errores
 		if err != nil{
 			log.Println(err)
+			cli.listo<- true //terminado
 			continue //hay que seguir escuchando pese a los errores
 		}
 		log.Println("Despachado::", msg)
+		cli.listo<- true //terminado
 	}
+
 }
 
 
 //Recive los mensajes del cliente y reconocer los comandos para ofrecer el procedimiento adecuado
 //deberia seraprace en dos partes ioServidor y servicios. Pero no hay tiempo por ahora.
+//cana comando deberia tener su propia funsion en el ioServidor.
 func (cli *Cliente) Interpretar(entrada string){
 	
 	comando := strings.Split(entrada, " ")
@@ -142,24 +153,30 @@ func (cli *Cliente) Interpretar(entrada string){
 	case Ps.C_UNIR_CANAL:
 		
 		if _, ok := cli.canales.lista[comando[1]]; !ok {	//true si el canal no existe
-			log.Println("El canal no existe, lo que existe es", cli.canales.lista)
-			cli.escribir<- Ps.S_UNIR_RECHAZADO //se responde el mensaje adecuado
+			log.Println("El cliente pidio unirce a un canal que no existe")
+			cli.escribir<- Ps.S_UNIR_RECHAZADO 
+			<-cli.listo //espero que cli.escribir se termine de procesar
+			return
 		}
+		
 		cli.canales.lista[comando[1]].unir<- cli //se registra al cliente en el canal
-		
+		<-cli.canales.lista[comando[1]].listo //verifico si el canal termino de registrarlo
 		cli.escribir<- Ps.S_UNIR_ACEPTADO	//se responde al cliente el mensaje adecuado
-		
+		<-cli.listo //espero que se termine de procesar 
 	
 	case Ps.C_ENVIAR_ARCHIVO:
 		
-		//se espera enviar canal archivo peso
+		//se espera el formato: enviar <espacio> canal <espacio> nombre-archivo <espacion> peso-archivo
 		if len(comando) != 4{
 			cli.escribir<- Ps.S_ENVIO_RECHAZADO
+			<-cli.listo //espero que se termine de procesar
+			return 
 		} 
 
 		if cli.canal == nil{
 			log.Println("El cliente no esta en un canal")
 			cli.escribir<- Ps.S_ENVIO_RECHAZADO
+			<-cli.listo //espero que se termine de procesar 
 			return
 		}
 
@@ -172,9 +189,11 @@ func (cli *Cliente) Interpretar(entrada string){
 		log.Println("Se presume entrada para el canal")
 		if cli.canal != nil{
 			cli.canal.escribir<- strings.ToUpper(entrada) //echo xd
+			<-cli.canal.listo //espero que se termine de procesar 
 		}else{
 			log.Println("El cliente no esta en un canal")
 			cli.escribir<- "no estas en un canal viejo..."
+			<-cli.listo //espero que se termine de procesar 
 		}
 	}
 	return
@@ -194,6 +213,7 @@ func main(){
 		escribir: make(chan string),
 		unir: make(chan *Cliente),
 		salir: make(chan *Cliente),
+		listo: make(chan bool),
 	}	
 
 	go canal1.Iniciar()
@@ -222,6 +242,7 @@ func handlerCliente(conn net.Conn, canales Canales){
 		conn: conn,
 		escribir: make(chan string),
 		canales: &canales,
+		listo:make(chan bool),
 		//canal //no registrado en un canal por ahora
 	}
 
@@ -229,98 +250,182 @@ func handlerCliente(conn net.Conn, canales Canales){
 	log.Println("Cliente conectado: ", cli.conn.RemoteAddr().String())
 
 	go cli.Escribir() //para enviar los mensajes al cliente
-	cli.Leer() //para recivir los mensajes del cliente
+	cli.Leer() //para recivir los mensajes del cliente, se cierra si la conexion se pierde
+	close(cli.escribir) //cerra el canal de escribir termina con la subrutina Leer()
 	log.Println("El cliente ",cli.conn.RemoteAddr().String(),"se desconecto")
 }
 
 
 //Funcion de prueba que maneja la subida de un archivo desde el cliente
 func recivirArchivo(cli *Cliente, entrada []string){
-	//El formato es enviar canal archivo peso
+	//se espera el formato: enviar <espacio> canal <espacio> nombre-archivo <espacion> peso-archivo
+	//a partir de ahora la comunicacion necesita sincronisacion
+	//no se usaran los canales de escuha del servidor
 			// canal := entrada[1]
 			nombreAr := entrada[2]
 			pesoAr := entrada[3] //el valor se recive en string
-
 			archivo, err := os.Create("servidor_archivos/"+nombreAr)
 			if err != nil{
 				log.Println(err)
-				cli.escribir<- Ps.S_ENVIO_RECHAZADO
+				cli.conn.Write([]byte(Ps.S_ENVIO_RECHAZADO))
 				return
 			}
 			defer archivo.Close() 
-			buffer := make([]byte, BUFFER_TAMANIO)
 			
-			cli.escribir<- Ps.S_ENVIO_APROBADO 
+			buffer := make([]byte, BUFFER_RCV_ARCHIVO) 
+			
+			cli.conn.Write([]byte(Ps.S_ENVIO_APROBADO)) //debe ser persistente, para eso hay que modificar al bucle que escribe asi que se ará más adelante
 
-			//tengo que escuchar la respuesta del cliente
 			log.Println("Esperando el archivo...")
-			
 			var cuenta int 
+			
+			//ahora el servidor está listo para recivir el archivo
+			//El servidor notificara <imposible continuar> apartir de ahora si ocurre un error de lectura
 			for {
 				n, err := cli.conn.Read(buffer)
 				cuenta+=n
 
-				if err != nil{
+				//en caso de error al leer de la conexión
+				// if err != nil{
+					if true{ //forzar el error
 					//EOF ocurre si hay una perdida de conexión
+					archivo.Close()
 					defer os.Remove("servidor_archivos/"+nombreAr) //se debe eliminar el archivo
 					log.Println(err)
-					return
+					//este error ocurre mientras el cliente sigue enviando datos
+					//cada stream que el cliente envie a partir de ahora es invalido
+					//hasta que el cliente y el servidor entiendan que deden iniciar un nuevo proceso
+					//por ahora cerrare la conexion por completo
+
+					//Saco al cliente del registro del canal
+					//elimino la referencia al canal desde el cliente
+					tempCanal := cli.canal
+					cli.canal.salir<-cli //saco al cliente del canal. Debería ser algo como canal.salir<- cli. Aparte se proceso cli.canal = nil
+					<-tempCanal.listo //tengo que recivir la notificación de que el canal termino de sacar al cliente
+					
+					//notifico al cliente
+					cli.escribir<- Ps.S_IMPOSIBLE_CONTINUAR //No puedo leer de la conexion, pero puedo escribir. Esto es TCP
+					<-cli.listo
+					cli.conn.Close() //elimino completamente al cliente
+					return //a partir de ahora el cliente esta desconectado y todos sus datos se eliminaron
 				}
-			
+
+				//en caso de error al escribir en el archivo creado
 				_, aerr := archivo.Write(buffer[:n])
 				if aerr != nil{
+					archivo.Close()
 					defer os.Remove("servidor_archivos/"+nombreAr) //eliminar el archivo porque no se pudo crear correctamente
 					log.Println(aerr)
+					cli.escribir<- Ps.S_IMPOSIBLE_CONTINUAR
+					<-cli.listo
 					return
 				}
 
-				if n < BUFFER_TAMANIO {
+				//si no se puede leer suficientes bytes es que finalisó la transmisión. Qué pasa si n == BUFFER_RCV_ARCHIVO pero la transmisión termino?
+				if n < BUFFER_RCV_ARCHIVO {
 					log.Println("Trasmision terminada, se presume recepcion completada")
-					// log.Println("Todos los bytes", buffer[:n])
 					archivo.Close()
 					break
 				}
 		}
-
-		//notifico al cliente que se recivo todo el archivo, se espera confirmacion para saber si el archivo fue enviado
-		cli.escribir<- Ps.S_ARCHIVO_RECIVIDO + " " + "Archivo recivido, bytes totales: " + fmt.Sprintf("%d",cuenta)
-			
+		//Recepción de archivo terminada, sin embargo el cliente pudo terminar la transmision a propósito
+		//para indicar que no pudo continuar durante la transmision. En tal caso el cliente envió información basura
+		//para cerrar el bucle de lectura. 
 		
-		//Esperar que el cliente envie la confirmacion de haber enviado el archvio completo
+		//el caso anterior se verifica con dos pasos: si bytes recividos == bytes esperados
+		//y si cliente envió el mensaje <imposible continuar>
+
+		//Comienso verificando la integridad del archivo recivido
+		log.Println("Bytes esperados,", pesoAr)
+		log.Println("Bytes recividos", cuenta)
+		if pesoAr != fmt.Sprintf("%d", cuenta) { //comparación de strings
+			
+			//el archivo se debe borrar pero antes hay que comunicar al cliente del error y este
+			//debe devolver el mensaje <imposible continuar>	
+			log.Println("Error. Los bytes recividos no son iguales a los bytes enviados por el cliente")
+			cli.escribir<- Ps.S_IMPOSIBLE_CONTINUAR + " " + "Los bytes no coinciden " + fmt.Sprintf("%d",cuenta)
+			<-cli.listo //espero que se termine de procesar 
+			log.Println("Esperando respuesta...")
+			//ahora se espera la respuesta del cliente con un deadline // cli.conn.SetReadDeadline(time)
+			rspCli := make([]byte, 128)
+			n, err := cli.conn.Read(rspCli)
+			if err != nil{
+				//persistencia hasta 3 intentos, si no se puede leer al cliente, le intento escribir <no puedo escuchar>
+				//si se escribe cierro la conexion
+				//el cliente debe dejar de enviar cualquier mensaje tambien y cerrar
+				//esto debe pasar si algún estremo no puede escuchar
+
+				os.Remove("servidor_archivos/"+nombreAr)
+				log.Println(err)
+				return 
+			}
+			temp := string(rspCli[:n])
+			rsp := strings.Split(temp, " ")
+			log.Println("recivido", temp)
+			if rsp[0] == Ps.C_IMPOSIBLE_CONTINUAR{
+				//confirmado que hubo un error desde el cliente
+				//borrar todo y dejar de esuchar
+				log.Println("El cliente confirmo que no pudo continuar")
+				os.Remove("servidor_archivos/"+nombreAr)
+				log.Println("Lado del servidor termino")
+				return 
+			}else{
+				log.Println("Respuesta inesperada del ciente",temp)
+				os.Remove("servidor_archivos/"+nombreAr)
+			}
+
+			return
+		}
+
+
+
+		//se le pregunta al cliente si logro enviar el 
+		//archivo correctamente. Si el cliente no puedo terminar enviara el mensaje <imposible continuar>
+
+		
+		//notifico al cliente que se reciviio la transmision, se espera confirmacion para saber si el archivo fue enviado
+		cli.escribir<- Ps.S_ARCHIVO_RECIVIDO + " " + "Archivo recivido, bytes totales: " + fmt.Sprintf("%d",cuenta)
+		<-cli.listo //espero que se termine de procesar 
+		log.Println("msg enviado...")	
+		
+		//Esperar que el cliente envíe la confirmación de haber enviado el archvio completo
 		log.Println("Esperando confirmacion del cliente...")
 		
 		b := make([]byte, 128)
 		n, err := cli.conn.Read(b)
 		if err != nil{
+			//persistencia hasta 3 intentos, si no se puede leer al cliente, le intento escribir <no puedo escuchar>
+			//si se escribe cierro la conexion
+			//el cliente debe dejar de envair cualquier mensaje tambien y cerrar
+			//esto debe pasar si algún estremo no puede escuchar
+
 			os.Remove("servidor_archivos/"+nombreAr)
 			log.Println(err)
-			return
+			return 
 		}
 
-		// log.Println("Debug:: bytes recividos", b[:n])
+		//durante el proceso de recepción de archivos no es necesario el \n
 		temp := string(b[:n])
-		// log.Println("Debug::temp:", temp)
 		recivido := strings.Trim(temp, "\n")
-		// log.Println("Debug::con triem", recivido)
 		if recivido != Ps.C_ARCHIVO_ENVIADO {
+			//si el cliente no envio un <archivo enviado> esto puede ser un
+			//<imposible continuar> o un <no puedo escuchar>
+			//en caso de imposible continuar cierro todo
 			log.Println("El cliente envio el siguiente mensaje:", recivido)
+			if recivido == Ps.C_IMPOSIBLE_CONTINUAR{
+				log.Println("El cliente no pudo enviar el archivo")
+				log.Println("Lado del servidor termino") //mejor un defer
+				//el archivo se va a borrar, dejo todo y termino este extremo
+				return
+			}
 			os.Remove("servidor_archivos/"+nombreAr)
 			return
 		}
+		//fin de prueba
 		log.Println("El cliente confirmo que envio el archivo")
 
-		//Verificar la integridad del archivo recivido
-		log.Println("Bytes esperados,", pesoAr)
-		log.Println("Bytes recividos", cuenta)
-		if pesoAr != fmt.Sprintf("%d", cuenta) { //comparación de strings
-				
-			log.Println("Error. Los bytes recividos no son iguales a los bytes enviados por el cliente")
-			cli.escribir<- Ps.S_IMPOSIBLE_CONTINUAR + " " + "Los bytes no coinciden" + fmt.Sprintf("%d",cuenta)
-			os.Remove("servidor_archivos/"+nombreAr) //mejor usar archivo.path() o algo asi
-			
-			return
-		}
+		
 
-		log.Println("Lado del servidor cerrado. Escuchando nuevas peticiones")
+		log.Println("Lado del servidor termino")
 			//fin de prueba
 }
